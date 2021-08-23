d151a6bf9d36f58943b7a4524a64fb576b61b8a7
HeyItsF1 <nengikaribibotoye@gmail.com>
2021-08-23 17:00:50 +0100

    Add findByUsername to PersonRepository

--- src/main/java/com/example/group6project/repository/PersonRepository.java
+++ src/main/java/com/example/group6project/repository/PersonRepository.java
@@ -4,6 +4,9 @@ import com.example.group6project.models.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PersonRepository extends JpaRepository<Person,Long> {
+    Optional<Person> findByUsername(String username);
 }
